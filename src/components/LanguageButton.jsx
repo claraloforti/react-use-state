aa77848c7921a79fcf80a32c4566e2095e6ab875
Clara Loforti <clara.loforti@gmail.com>
2026-01-21 14:32:22 +0100

    mostro i bottoni in pagina

---
+++ src/components/LanguageButton.jsx
@@ -0,0 +1,5 @@
+const LanguageBtn = ({ title }) => {
+    return <button>{title}</button>;
+};
+
+export default LanguageBtn
--- src/components/LanguagesList.jsx
+++ src/components/LanguagesList.jsx
@@ -1,8 +1,21 @@
 import languages from "../assets/languages";
+import LanguageBtn from "./LanguageButton";
 
 const LanguagesList = () => {
+
     return (
-        <h1>Learn Web Development</h1>
+        <>
+            <h1>Learn Web Development</h1>
+
+            <div className="languages-list">
+                {languages.map((language) => (
+                    <LanguageBtn
+                        key={language.id}
+                        title={language.title}
+                    />
+                ))}
+            </div>
+        </>
     )
 };
 

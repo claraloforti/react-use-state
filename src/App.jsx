97a3bc71a89cf0274d855c56c22977c67bbf7a68
Clara Loforti <clara.loforti@gmail.com>
2026-01-21 14:14:48 +0100

    creo LanguagesList.jsx e aggiungo titolo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import LanguagesList from "./components/LanguagesList";
+
+function App() {
+  return (
+    <LanguagesList />
+  )
+}
+
+export default App
---
+++ src/components/LanguagesList.jsx
@@ -0,0 +1,13 @@
+import languages from "../assets/languages";
+
+const LanguagesList = () => {
+    return (
+        <h1>Learn Web Development</h1>
+    )
+};
+
+
+
+
+export default LanguagesList;
+
